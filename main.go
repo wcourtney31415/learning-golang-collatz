ab4d72dbf680d6d1cf26d3e7ce458c4843ddb81b
wcourtney31415 <wcourtney31415@gmail.com>
2021-09-20 02:56:19 -0400

    Used better methods to reduce line count while maintaining readability.

--- main.go
+++ main.go
@@ -9,17 +9,14 @@ func main() {
 	var myNumber int
 	fmt.Scan(&myNumber)
 	list := []int{myNumber}
-	// list = append(list, myNumber)
 	var lastElement = myNumber
 	for lastElement != 2 {
 		lastElement = list[len(list)-1]
 		var calc int
 		if lastElement%2 == 0 {
 			calc = lastElement / 2
-			// list = append(list, even)
 		} else {
 			calc = lastElement*3 + 1
-			// list = append(list, odd)
 		}
 		list = append(list, calc)
 	}
